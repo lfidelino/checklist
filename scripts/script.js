3c7d3b18682cc69d2f7f6b6f4df3bdaf71aea474
Lawrence Fidelino <lfidelino@gmail.com>
2021-02-02 13:00:46 +0800

    Convert double quotes to single quotes in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,33 @@
-var inputText = document.getElementById("input__text");
-var btnAdd = document.getElementById("button__add");
-var checklist = document.getElementById("section__checklist");
+var inputText = document.getElementById('input__text');
+var btnAdd = document.getElementById('button__add');
+var checklist = document.getElementById('section__checklist');
 
 inputText.focus();
 
 var addChecklistItem = () => {
   if (inputText.value) {
     // NOTE Create elements
-    var checklistItem = document.createElement("div");
-    checklistItem.classList = "checklistItem grid grid-item py-1";
+    var checklistItem = document.createElement('div');
+    checklistItem.classList = 'checklistItem grid grid-item py-1';
 
-    var item = document.createElement("div");
+    var item = document.createElement('div');
     var textItem = document.createTextNode(inputText.value.trim());
-    item.classList = "px-2";
+    item.classList = 'px-2';
 
-    var edit = document.createElement("div");
-    edit.classList = "flex justify-center hover:text-green-600";
-    var iconEdit = document.createElement("i");
-    iconEdit.classList = "far fa-edit";
+    var edit = document.createElement('div');
+    edit.classList = 'flex justify-center hover:text-green-600';
+    var iconEdit = document.createElement('i');
+    iconEdit.classList = 'far fa-edit';
 
-    var remove = document.createElement("div");
-    remove.classList = "flex justify-center hover:text-red-600";
-    var iconRemove = document.createElement("i");
-    iconRemove.classList = "far fa-trash-alt";
+    var remove = document.createElement('div');
+    remove.classList = 'flex justify-center hover:text-red-600';
+    var iconRemove = document.createElement('i');
+    iconRemove.classList = 'far fa-trash-alt';
 
     // NOTE Add event listeners
-    item.addEventListener("click", () => linethroughItem(item));
-    edit.addEventListener("click", () => editItem(edit));
-    remove.addEventListener("click", () => removeItem(remove));
+    item.addEventListener('click', () => linethroughItem(item));
+    edit.addEventListener('click', () => editItem(edit));
+    remove.addEventListener('click', () => removeItem(remove));
 
     // NOTE Append elements
     item.appendChild(textItem);
@@ -39,54 +39,54 @@ var addChecklistItem = () => {
     checklistItem.appendChild(remove);
 
     checklist.appendChild(checklistItem);
-    document.documentElement.style.setProperty("--animate-duration", "0.25s");
-    checklistItem.classList.add("animate__animated");
-    checklistItem.classList.add("animate__zoomIn");
+    document.documentElement.style.setProperty('--animate-duration', '0.25s');
+    checklistItem.classList.add('animate__animated');
+    checklistItem.classList.add('animate__zoomIn');
     setTimeout(() => {
-      checklistItem.classList.remove("animate__animated");
-      checklistItem.classList.remove("animate__zoomIn");
+      checklistItem.classList.remove('animate__animated');
+      checklistItem.classList.remove('animate__zoomIn');
     }, 250);
   }
 
-  inputText.value = "";
+  inputText.value = '';
   inputText.focus();
 };
 
 // NOTE Event functions
 var linethroughItem = (item) => {
-  item.classList.toggle("line-through");
-  item.parentElement.classList.toggle("done");
+  item.classList.toggle('line-through');
+  item.parentElement.classList.toggle('done');
 };
 
 var editItem = (edit) => {
   var oldText = edit.parentElement.childNodes[0].innerText;
-  var text = prompt("Edit your checklist item.", edit.parentElement.childNodes[0].innerText);
+  var text = prompt('Edit your checklist item.', edit.parentElement.childNodes[0].innerText);
   if (text && text != oldText) {
     edit.parentElement.childNodes[0].innerText = text;
-    document.documentElement.style.setProperty("--animate-duration", "0.15s");
-    edit.parentElement.classList.add("animate__animated");
-    edit.parentElement.classList.add("animate__pulse");
+    document.documentElement.style.setProperty('--animate-duration', '0.15s');
+    edit.parentElement.classList.add('animate__animated');
+    edit.parentElement.classList.add('animate__pulse');
     setTimeout(() => {
-      edit.parentElement.classList.remove("animate__animated");
-      edit.parentElement.classList.remove("animate__pulse");
+      edit.parentElement.classList.remove('animate__animated');
+      edit.parentElement.classList.remove('animate__pulse');
     }, 150);
   }
 };
 
 var removeItem = (remove) => {
-  var confirmRemove = window.confirm("Delete checklist item?");
+  var confirmRemove = window.confirm('Delete checklist item?');
 
   if (confirmRemove) {
-    document.documentElement.style.setProperty("--animate-duration", "0.25s");
-    remove.parentElement.classList.add("animate__animated");
-    remove.parentElement.classList.add("animate__fadeOutUp");
+    document.documentElement.style.setProperty('--animate-duration', '0.25s');
+    remove.parentElement.classList.add('animate__animated');
+    remove.parentElement.classList.add('animate__fadeOutUp');
     setTimeout(() => remove.parentElement.remove(), 250);
   }
 };
 
 // NOTE Add event listeners
-btnAdd.addEventListener("click", () => addChecklistItem());
+btnAdd.addEventListener('click', () => addChecklistItem());
 
-inputText.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") addChecklistItem();
+inputText.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') addChecklistItem();
 });
